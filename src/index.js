0720fff8beb155fecd4dde0d1cfffff61253f015
zeteo_xulon <n.dreux@live.fr>
2022-11-12 12:33:28 +0100

    installed react router but need to be configurated // created server json files // created SCSS tree components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
