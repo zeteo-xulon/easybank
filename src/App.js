37d4abd7e33da693870109c655f1fc7db3097de5
zeteo_xulon <n.dreux@live.fr>
2022-11-12 20:59:07 +0100

    Added React Router DOM // Created Page404 and Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Page404 from "./components/Page404";
+
 export default function App() {
   return (
     <div className="app">
-      
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Page404/index.jsx
@@ -0,0 +1,7 @@
+export default function Page404() {
+  return (
+    <div className='page-404'>
+      
+    </div>
+  );
+}
