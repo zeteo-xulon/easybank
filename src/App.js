b9d899066334291d9610f795842515b2167a2e79
zeteo_xulon <n.dreux@live.fr>
2022-11-23 16:30:08 +0100

    Added Contact component // imported contact style to index

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Contact from './components/Contact'
 import Page404 from "./components/Page404";
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/about" element={<Home />} />
-        <Route exact path="/contact" element={<Home />} />
+        <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/blog" element={<Home />} />
         <Route exact path="/career" element={<Home />} />
         <Route path="*" element={<Page404 />} />
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+export default function Contact() {
+  const [title, setTitle] = useState('');
+  const [text, setText] = useState('');
+
+  function submit(){
+    if(title.length === 0 || title.length < 2) { return alert("The title need to be more than 2 caracteres") }
+    if(text.length === 0 || text.length < 2) { return alert("The text need to be more than 2 caracteres") }
+    setTitle('');
+    setText('');
+    return alert("Your message has been send.")
+  }
+
+
+  return (
+    <section className="contact">
+
+      <div className="contact__text-container">
+        <h1 className="contact__title">Contact us</h1>
+        <p className="contact__text">You can contact us by fulfilling this form.</p>
+      </div>
+
+      <form className="contact__form">
+
+        <div className="contact__form__text-container">
+          <label className="contact__form__title">Title of your message :</label>
+          <input 
+          type="text" 
+          className="contact__form__input-title" 
+          id="contactTitle"
+          value={title}
+          onChange={(e)=> { setTitle(e.target.value) }}/>
+        </div>
+        
+        <div className="contact__form__text-container">
+          <label className="contact__form__title">Text of your message :</label>
+          <textarea 
+          type="text" 
+          className="contact__form__input-textarea" 
+          id="contactText" 
+          value={text}
+          onChange={(e)=> { setText(e.target.value) }}
+          />
+        </div>
+        
+      </form>
+      
+      <button className="contact__btn" id="contactBtn" onClick={()=>{submit()}}>Envoyer</button>
+
+    </section>
+  );
+}
