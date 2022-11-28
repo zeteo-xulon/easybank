23abc95c6a53fdefc69a14518473fe2ef8776971
zeteo_xulon <n.dreux@live.fr>
2022-11-28 12:18:20 +0100

    working on resolving build deployment

--- src/components/Careers/index.jsx
+++ src/components/Careers/index.jsx
@@ -1,4 +1,4 @@
-import {ReactComponent as ArrowDown} from '../../assets/images/arrow_down.svg';
+
 
 const textList = [
   {
@@ -40,7 +40,7 @@ export default function Careers() {
       <article className="careers__intro">
         <h1 className="careers__intro__title">Work at EasyBank</h1>
         <p className="careers__intro__sub">We’re building banking for startups. We prioritize beauty, usability, and simplicity.</p>
-        <a href="#openroles">see our open roles <ArrowDown /></a>
+        <a href="#openroles">see our open roles</a>
         <p className="careers__intro__text">EasyBank is a financial technology company, and a bank. Banking services provided by Choice Financial Group and Evolve Bank & Trust®; Members GMAA.</p>
       </article>
 
--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function Contact() {
   const [title, setTitle] = useState('');
